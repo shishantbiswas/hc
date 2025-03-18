6e8d770d72a657adb644fe57cf16778b9ea32855
Shishant Biswas <shishantbiswas@gmail.com>
2025-03-18 08:24:43 +0530

    neon adapter integreation

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
+
+const PrismaSingleton = () => {
+  const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaNeon(neon);
+  return new PrismaClient({ adapter });
+};
 
 declare global {
-  var prisma: PrismaClient | undefined;  // eslint-disable-line
+  var prisma: PrismaClient | undefined; // eslint-disable-line
 }
-export const db = globalThis.prisma || new PrismaClient();
+export const db = globalThis.prisma ?? PrismaSingleton();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
