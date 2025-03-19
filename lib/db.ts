9f35275b1683a5e6d860a212807dd0f8c4400f99
Shishant Biswas <shishantbiswas@gmail.com>
2025-03-19 09:10:36 +0530

    fix: disable neon driver for performance issues

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { Pool } from "@neondatabase/serverless";
+// import { PrismaNeon } from "@prisma/adapter-neon";
+// import { Pool } from "@neondatabase/serverless";
+// "@neondatabase/serverless": "^0.10.4"
+
 
 const PrismaSingleton = () => {
-  const neon = new Pool({ connectionString: process.env.DATABASE_URL });
-  const adapter = new PrismaNeon(neon);
-  return new PrismaClient({ adapter });
+  // const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+  // const adapter = new PrismaNeon(neon);
+  return new PrismaClient(); // disable neon driver for performance issues
 };
 
 declare global {
